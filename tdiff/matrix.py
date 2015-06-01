671e5deb5a8db2a90c49dfefa511470cffe1ab34
David McClure <davidwilliammcclure@gmail.com>
2015-06-01 17:00:33 -0400

    First pass on A vs. B, one-to-one term comparisons.

--- tdiff/matrix.py
+++ tdiff/matrix.py
@@ -40,7 +40,7 @@ class Matrix:
         self.text2 = text2
 
 
-    def index(self, n, **kwargs):
+    def compare(self, n, **kwargs):
 
         """
         Index KDE similarities between top N types in each text.
@@ -52,15 +52,30 @@ class Matrix:
         mft1 = self.text1.most_frequent_terms(n)
         mft2 = self.text2.most_frequent_terms(n)
 
+        terms = set.union(mft1, mft2)
+        pairs = combinations(terms, 2)
+        count = comb(len(terms), 2)
+
+        links = []
         for t1 in mft1:
+
+            st1 = self.text1.unstem(t1)
 
             scores = []
             for t2 in mft2:
+
+                st2 = self.text2.unstem(t2)
 
                 t1_kde = self.text1.kde(t1, **kwargs)
                 t2_kde = self.text2.kde(t2, **kwargs)
 
                 score = 1-distance.braycurtis(t1_kde, t2_kde)
-                scores.append((t2, score))
+                scores.append((st2, score))
 
-            # TODO
+            scores = sorted(scores, key=lambda x: x[1], reverse=True)
+
+            if st1 != scores[0][0]:
+                links.append((st1, scores[0][0], scores[0][1]))
+
+        links = sorted(links, key=lambda x: x[2], reverse=True)
+        return links
